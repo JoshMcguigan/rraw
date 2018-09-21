8234b4c5ce7a488a07fc3f4b18557a0394230eb6
Josh Mcguigan <joshmcg88@gmail.com>
2018-09-21 04:03:58 -0700

    Improved integration testing

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,6 +5,7 @@ use std::time::SystemTime;
 use rraw::RRAWResult;
 use std::thread;
 use std::time;
+use rraw::Client;
 
 // NOTE: new accounts are only allowed to post once per 10 minutes, so these tests could fail if run repeatedly
 
@@ -22,23 +23,30 @@ fn try_new_error() {
 }
 
 #[test]
-fn test() -> RRAWResult<()> {
-    let reddit_user_agent = dotenv::var("REDDIT_USER_AGENT").unwrap();
-    let reddit_username = dotenv::var("REDDIT_USERNAME").unwrap();
-    let reddit_password = dotenv::var("REDDIT_PASSWORD").unwrap();
-    let reddit_client_id = dotenv::var("REDDIT_CLIENT_ID").unwrap();
-    let reddit_client_secret = dotenv::var("REDDIT_CLIENT_SECRET").unwrap();
+fn write_comment() -> RRAWResult<()> {
+    let reddit_client = test_client()?;
 
-    let reddit_client = rraw::Client::try_new(
-        &reddit_username,
-        &reddit_password,
-        &reddit_client_id,
-        &reddit_client_secret,
-        &reddit_user_agent,
-    )?;
+    let timestamp = timestamp();
+    let comment_text = format!("This is a test comment - {:?}", timestamp);
+    reddit_client.reply("t3_9hlk2e", &comment_text)?;
+    thread::sleep(time::Duration::from_millis(15000));
+    let comments = reddit_client.comments("test", "9hlk2e")?;
+
+    for comment in comments {
+        if comment_text == comment.body {
+            return Ok(())
+        }
+    }
+
+    panic!("Comment not found")
+}
+
+#[test]
+fn test() -> RRAWResult<()> {
+    let reddit_client = test_client()?;
 
     let subreddit = "test";
-    let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    let timestamp = timestamp();
     let post_title = format!("Testing RRAW - {:?}", timestamp);
     let post_body = format!("Post body - {:?}", timestamp);
 
@@ -52,11 +60,32 @@ fn test() -> RRAWResult<()> {
     let comment_text = "This is a test comment.";
     reddit_client.reply(&response.name, comment_text)?;
 
-    // todo add testing of comments route
-    //    let comments = reddit_client.comments(subreddit, &response.id)?;
-    //
-    //    assert_eq!(1, comments.len(), "Expected comment to be posted and returned");
-    //    assert_eq!(comment_text, comments[0].body);
+    thread::sleep(time::Duration::from_millis(15000)); // todo improve this
+
+    let comments = reddit_client.comments(subreddit, &response.id)?;
+
+    assert_eq!(1, comments.len(), "Expected comment to be posted and returned");
+    assert_eq!(comment_text, comments[0].body);
 
     Ok(())
+}
+
+fn test_client() -> RRAWResult<Client> {
+    let reddit_user_agent = dotenv::var("REDDIT_USER_AGENT").unwrap();
+    let reddit_username = dotenv::var("REDDIT_USERNAME").unwrap();
+    let reddit_password = dotenv::var("REDDIT_PASSWORD").unwrap();
+    let reddit_client_id = dotenv::var("REDDIT_CLIENT_ID").unwrap();
+    let reddit_client_secret = dotenv::var("REDDIT_CLIENT_SECRET").unwrap();
+
+    rraw::Client::try_new(
+        &reddit_username,
+        &reddit_password,
+        &reddit_client_id,
+        &reddit_client_secret,
+        &reddit_user_agent,
+    )
+}
+
+fn timestamp() -> u64 {
+    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
 }
